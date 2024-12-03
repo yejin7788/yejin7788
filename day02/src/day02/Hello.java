867a5a346dd5d3261dac45d0459fac40e9cca95d
yejin7788 <yejin777888@gmail.com>
2024-12-03 11:00:44 +0900

    2일차

---
+++ day02/src/day02/Hello.java
@@ -0,0 +1,12 @@
+//클래스의 위치(집 주소)
+package day02;
+//클래스영역(집)
+public class Hello {
+	//메소드 영역(방)
+	public static void main(String[] args) {		
+		//프로그램 실행 시 나올 내용에 대한 코드
+		//출력 명령
+		System.out.println("Hello World");
+		System.out.println("Hello Java!");
+	}
+}
